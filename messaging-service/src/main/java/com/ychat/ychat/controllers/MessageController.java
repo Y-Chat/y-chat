24809dedd360de2c259e47a7c3218836ef21c67c
Ben Strobel <benedikt.strobel@tum.de>
2024-01-10 07:04:44 +0100

    implemented basic messaging logic

--- messaging-service/src/main/java/com/ychat/ychat/controllers/MessageController.java
+++ messaging-service/src/main/java/com/ychat/ychat/controllers/MessageController.java
@@ -2,18 +2,28 @@ package com.ychat.ychat.controllers;
 
 import com.openapi.gen.messaging.api.MessageApi;
 import com.openapi.gen.messaging.dto.Message;
+import com.ychat.ychat.services.MessagingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("${openapi.yChatMessaging.base-path:}")
 public class MessageController implements MessageApi {
 
+    private final MessagingService messagingService;
+
+    public MessageController(@Autowired(required = true) MessagingService messagingService) {
+        this.messagingService = messagingService;
+    }
+
     @Override
     public ResponseEntity<Message> sendMessage(com.openapi.gen.messaging.dto.Message message) {
-        return MessageApi.super.sendMessage(message);
+        var res = messagingService.sendMessage(message, UUID.randomUUID());
+        return res.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(500).build());
     }
 }
--- messaging-service/src/main/java/com/ychat/ychat/services/MessagingService.java
+++ messaging-service/src/main/java/com/ychat/ychat/services/MessagingService.java
@@ -3,12 +3,15 @@ package com.ychat.ychat.services;
 import com.ychat.ychat.repositories.ChatMessageRepository;
 import com.openapi.gen.messaging.dto.Message;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class MessagingService {
@@ -16,9 +19,21 @@ public class MessagingService {
     @Autowired
     private ChatMessageRepository messageRepository;
 
-    public Optional<List<Message>> getMessages(UUID chatId, LocalDateTime fromDate) {
+    public Pair<Optional<List<Message>>, PageRequest> getMessages(UUID chatId, LocalDateTime fromDate, Integer page, Integer pageSize) {
+        if(page == null) {
+            page = 0;
+        }
+        if(pageSize == null) {
+            pageSize = 20;
+        }
 
-        return Optional.empty();
+        PageRequest pageRequest = PageRequest.of(page, pageSize);
+        var res = messageRepository
+                .findByChatId(chatId, PageRequest.of(page, pageSize))
+                .stream()
+                .map(com.ychat.ychat.models.Message::toOpenAPI)
+                .collect(Collectors.toList());
+        return Pair.of(Optional.of(res), pageRequest);
     }
 
     public Optional<Message> sendMessage(Message message, UUID senderId) {
