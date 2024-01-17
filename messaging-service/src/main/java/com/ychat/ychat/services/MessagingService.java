a5c5ccfd13a867021cf4bfe8fee0d63d6e856f6c
Ben Strobel <benedikt.strobel@tum.de>
2024-01-17 12:46:00 +0100

    Fixed Spring Kafka Setup (notification & messaging service), Can now be used as reference for other services

--- messaging-service/src/main/java/com/ychat/ychat/services/MessagingService.java
+++ messaging-service/src/main/java/com/ychat/ychat/services/MessagingService.java
@@ -2,8 +2,11 @@ package com.ychat.ychat.services;
 
 import com.asyncapi.gen.notification.model.AnonymousSchema1;
 import com.asyncapi.gen.notification.model.Notification;
+import com.ychat.ychat.controllers.MessagesController;
 import com.ychat.ychat.repositories.ChatMessageRepository;
 import com.openapi.gen.messaging.dto.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.util.Pair;
@@ -17,6 +20,8 @@ import java.util.stream.Collectors;
 
 @Service
 public class MessagingService {
+
+    private static final Logger logger = LoggerFactory.getLogger(MessagingService.class);
 
     @Autowired
     private ChatMessageRepository messageRepository;
--- notification-service/src/main/java/com/ychat/ychat/controllers/NotificationController.java
+++ notification-service/src/main/java/com/ychat/ychat/controllers/NotificationController.java
@@ -1,25 +1,26 @@
 package com.ychat.ychat.controllers;
 
 import com.asyncapi.gen.notification.model.Notification;
+import com.asyncapi.gen.notification.service.MessageHandlerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
-public class NotificationController {
+public class NotificationController extends MessageHandlerService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(NotificationController.class);
 
-    @KafkaListener(topics = "notification/notification")
+
+    @Override
     public void createNotification(@Payload Notification payload,
                                    @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
                                    @Header(KafkaHeaders.RECEIVED_KEY) Integer key,
                                    @Header(KafkaHeaders.RECEIVED_PARTITION) int partition,
                                    @Header(KafkaHeaders.RECEIVED_TIMESTAMP) long timestamp) {
-        LOGGER.info("Test Function - Key: " + key + ", Payload: " + payload.toString() + ", Timestamp: " + timestamp + ", Partition: " + partition);
+        LOGGER.info("Helper: Key: " + key + ", Payload: " + payload.toString() + ", Timestamp: " + timestamp + ", Partition: " + partition);
     }
 }
