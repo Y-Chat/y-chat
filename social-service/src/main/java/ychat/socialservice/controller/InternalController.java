24017362d21d995714a915df857c42c57e2b9751
Ben Strobel <benedikt.strobel@tum.de>
2024-02-03 18:54:04 +0100

    Added internal chat member getter

--- social-service/src/main/java/ychat/socialservice/controller/InternalController.java
+++ social-service/src/main/java/ychat/socialservice/controller/InternalController.java
@@ -3,10 +3,16 @@ package ychat.socialservice.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import ychat.socialservice.SecurityConfig;
+import ychat.socialservice.service.ChatService;
 import ychat.socialservice.service.InternalService;
+import ychat.socialservice.service.dto.ChatMemberDTO;
 
 import java.util.UUID;
 
@@ -22,8 +28,11 @@ import java.util.UUID;
 public class InternalController {
     private final InternalService internalService;
 
-    public InternalController(@NonNull InternalService internalService) {
+    private final ChatService chatService;
+
+    public InternalController(@NonNull InternalService internalService, @Autowired ChatService chatService) {
         this.internalService = internalService;
+        this.chatService = chatService;
     }
 
     @GetMapping("/{userId}/shouldReceive/{chatId}")
@@ -33,5 +42,17 @@ public class InternalController {
     )
     public boolean shouldReceive(@PathVariable UUID userId, @PathVariable UUID chatId) {
         return internalService.shouldReceive(userId, chatId);
+    }
+
+    @GetMapping("/{chatId}/members")
+    @Operation(
+            summary = "Fetch all members of a chat.",
+            description = "Returns the user ids, profiles, and if it is a group chat, also the roles " +
+                    "of each member. Page size is not allowed to exceed " +
+                    ChatService.MAX_CHAT_MEMBER_PAGE_SIZE + "."
+    )
+    public Page<ChatMemberDTO> getChatMembers(@PathVariable UUID chatId, @RequestParam UUID userId,
+                                              Pageable pageable) {
+        return chatService.getChatMembers(chatId, userId, pageable);
     }
 }
