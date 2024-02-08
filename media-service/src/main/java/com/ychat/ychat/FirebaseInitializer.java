8d903fac88da79b4639d58ab863d8671f100877a
Ben Strobel <benedikt.strobel@tum.de>
2024-02-08 15:44:42 +0100

    Added social service connector to media service & checking if user is allowed to access media from a chat

--- media-service/src/main/java/com/ychat/ychat/FirebaseInitializer.java
+++ media-service/src/main/java/com/ychat/ychat/FirebaseInitializer.java
@@ -8,9 +8,11 @@ import com.google.cloud.storage.StorageException;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.StorageClient;
+import com.ychat.ychat.services.SocialServiceConnector;
 import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -18,12 +20,21 @@ import org.springframework.web.server.ResponseStatusException;
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class FirebaseInitializer {
 
     private static final Logger logger = LoggerFactory.getLogger(FirebaseInitializer.class);
+
+    private final SocialServiceConnector socialServiceConnector;
+
+    public FirebaseInitializer(@Autowired SocialServiceConnector socialServiceConnector) {
+        this.socialServiceConnector = socialServiceConnector;
+    }
 
     @PostConstruct
     public void onStart() {
@@ -47,7 +58,9 @@ public class FirebaseInitializer {
         FirebaseApp.initializeApp(options);
     }
 
-    public String generateSignedUrl(String objectName) {
+    Pattern chatIdPattern = Pattern.compile("chats/(.*?)/");
+
+    public String generateSignedUrl(String objectName, UUID requesterId) {
         try {
             Bucket bucket = StorageClient.getInstance().bucket();
             BlobInfo blobInfo = BlobInfo.newBuilder(bucket.asBucketInfo(), objectName).build();
@@ -57,7 +70,12 @@ public class FirebaseInitializer {
             }
 
             if (objectName.startsWith("chats/")) {
-                // TODO check from social service if user is part of chat otherwise return 403
+                Matcher matcher = chatIdPattern.matcher(objectName);
+                if(!matcher.find()) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "objectName format is invalid");
+                String chatId = matcher.group(1);
+                if(!socialServiceConnector.canUserAccessChat(requesterId, UUID.fromString(chatId))) {
+                    throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+                }
             }
             return blob.signUrl(2, TimeUnit.HOURS).toString();
         } catch (StorageException e) {
